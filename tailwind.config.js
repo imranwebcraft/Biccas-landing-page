e12ff44065322091ae5dd17bd981f2b3e7bc183e
imranwebcraft <imranhossainsakil22@gmail.com>
2023-12-29 21:25:46 +0600

    project setup with google font, react-router-dom and tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#54BD95',
+				textBlack: '#000000',
+				textBlack2: '#191A15',
+				textGray: '#A6A6A6',
+			},
+			fontFamily: {
+				primary: ['Inter', 'sans-serif'],
+			},
+		},
+		screens: {
+			xs: '480px',
+			ss: '620px',
+			sm: '768px',
+			md: '1060px',
+			lg: '1200px',
+			xl: '1700px',
+		},
+	},
+	plugins: [],
+};
